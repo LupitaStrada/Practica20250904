aacd2a7bd8969e6cb06f8ded5ea931fbf9d42d49
Lupita <lupitastrada1002@gmail.com>
2025-04-10 14:18:38 -0600

    actualizar vistas

--- Practica20250904.AppWebMVC/Models/Cliente.cs
+++ Practica20250904.AppWebMVC/Models/Cliente.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Practica20250904.AppWebMVC.Models;
 
@@ -7,11 +9,13 @@ public partial class Cliente
 {
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "El campo Nombre es obligatorio.")]
     public string Nombre { get; set; } = null!;
-
+    [Required(ErrorMessage = "El campo Apellido es obligatorio.")]
     public string Apellido { get; set; } = null!;
 
+    [DisplayName("Dirección")]
     public string? Direccion { get; set; }
-
+    [DisplayName("Teléfono")]
     public string? Telefono { get; set; }
 }
